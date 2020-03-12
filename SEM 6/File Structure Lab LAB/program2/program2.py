8b9efb3c71127b130fccd894a5ebeb6cb4290ea0
dntandan <deepnarayan006@gmail.com>
2020-03-12 23:39:22 +0530

    Sem 6 File Structure Program 2

---
+++ SEM 6/File Structure Lab LAB/program2/program2.py
@@ -0,0 +1,80 @@
+details = []
+
+
+class student:
+    def __init__(self, usn, name, sem):
+        self.usn = usn
+        self.name = name
+        self.sem = sem
+
+    def display_data(self):
+        print(f"USN -> {self.usn} \nName -> {self.name} \nSem -> {self.sem} \n")
+
+    def pack(self, file):
+        buffer = self.usn + "|" + self.name + "|" + self.sem + "|"
+        if len(buffer) > 45:
+            print("Length Exceeded The Buffer")
+        while len(buffer) < 46:
+            buffer += "_"
+        buffer += "\n"
+        file.write(buffer)
+
+    def modify_name(self):
+        newname = input("Enter The New Name ==>\t")
+        self.name = newname
+
+    def modify_sem(self):
+        newsem = input("Enter The New Sem ==>\t")
+        self.sem = newsem
+
+
+def unpack():
+    with open("students.txt", "r") as fp:
+        for line in fp:
+            fields = line.strip("_\n").split("|")[:-1]
+            details.append(student(fields[0], fields[1], fields[2]))
+
+
+def search():
+    for x in details:
+        if key == x.usn:
+            print("\n----Record Found----\n")
+            x.display_data()
+            option = input(
+                "Select The Data To Modify\n1.Name\n2.Sem\n3.Do'nt Modify Anything ==>\t"
+            )
+            if option == "1":
+                x.modify_name()
+            elif option == "2":
+                x.modify_sem()
+            elif option == "3":
+                break
+    else:
+        print("\n----Record Not Found----\n")
+
+
+while True:
+    choice = input(
+        "\n1.Insert Student Details\n2.Search and Modify Student Details\n3.Exit\nEnter Your Choice ==>\t"
+    )
+    if choice == "1":
+        usn = input("Enter USN \t")
+        name = input("Enter Name \t")
+        sem = input("Enter Sem \t")
+        new_student = student(usn, name, sem)
+        with open("students.txt", "a+") as fp:
+            new_student.pack(fp)
+        print("\n---- Entered Data ----")
+        new_student.display_data()
+    elif choice == "2":
+        details = []
+        unpack()
+        key = input("Enter The USN To Search & Modify ==>\t")
+        search()
+        with open("students.txt", "w+") as fp:
+            for x in details:
+                x.pack(fp)
+    elif choice == "3":
+        break
+    else:
+        print("\n----Invalid Data Entered! Please Try Again----\n")
